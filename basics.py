422ac220a5fca355e89ef6b51499fb4e11ef0c3e
josephredmond5 <josephredmond5@icloud.com>
2024-02-11 00:22:04 +0000

    basics of pythons, first 2 lessons complete

---
+++ basics.py
@@ -0,0 +1,41 @@
+# # execution order
+# print('line 1') # this will be ignored
+# print('line 2')
+# print('line 3')
+
+# #data types
+# print('words') # strings - with single or double quotes
+# print(123) # Integers / Ints
+# print(-10)
+# print(1.5) # floating point value / float
+
+# # operations
+# print(3 + 3)
+# print(3 - 3)
+# print(3 * 3)
+# print(3 / 3)
+# # print(10 + 'hello') # this is not possible
+# print(10 * 'hello')
+# Integers & floating point numbers can be added together
+# Integers & Strings CANNOT be added together
+
+# Variables
+# test_value = 123
+# test_value += 50
+# print(test_value)
+
+
+# Inputs
+# user_input = input("please write something here -> ") # this will display the text then check for user input
+# print(user_input) # printing the variable lets you take charge of the input
+# The input got the value of test and this value was stored in the variable and then we print the value 
+ 
+ 
+# Greeter App
+
+# Ask the user for their name
+name = input("what is your name? ")
+
+# Print the greeting message
+print("hello", name, "have a nice day")
+print("hello " + name + " have a nice day") # can do it either way
